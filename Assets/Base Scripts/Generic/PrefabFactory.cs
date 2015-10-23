13f604714af524e0c67aeddeace78e9d3ff314f2
llmp <l@Mackenzie.local>
2015-10-22 18:08:00 -0700

    Added painter and cop now “sees” the player

---
+++ Assets/Base Scripts/Generic/PrefabFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabFactory : MonoBehaviour {
+	
+	public GameObject[] prefabArray = new GameObject[4];
+	public bool instanceExists = false;
+
+	void Start(){
+
+	}
+
+	public void instantiatePrefab(GameObject prefab, Vector3 position){
+		Instantiate(prefab,position,Quaternion.identity);
+	}
+
+	public void instantiatePrefabBornToDie(GameObject prefab, Vector3 position, float lifeSpan){
+		Object prefabInstance = Instantiate(prefab,position,Quaternion.identity);
+		instanceExists = true;
+		StartCoroutine(yieldDestroy(prefabInstance,lifeSpan));
+	}
+
+	void OnBecameInvisible(){
+		Destroy(this.gameObject);
+	}
+
+	IEnumerator yieldDestroy(Object prefabInstance, float lifeSpan){
+		Destroy(prefabInstance,lifeSpan);
+		yield return new WaitForSeconds(lifeSpan);
+		instanceExists = false;
+	}
+
+}
--- Assets/Characters/Painter/Scripts/PainterMovement.cs
+++ Assets/Characters/Painter/Scripts/PainterMovement.cs
@@ -2,101 +2,104 @@
 using System.Collections;
 using System;
 
-public class CopMovement : MonoBehaviour {
-	
-//	public SpriteRenderer spriteRenderer;
-	public float moveSpeed = 0.02f;
+public class PainterMovement : MonoBehaviour {
 
+	//	public SpriteRenderer spriteRenderer;
+	public float moveSpeed = 0.02f;
+	
 	[SerializeField]
 	private short moveCount = 0;
 	[SerializeField]
 	private short frameCount = 180;
-	[SerializeField]
-	private short fieldOfViewFront = 4;
-	[SerializeField]
-	private float fieldOfViewSides = 0.3f;
-
+	
 	private Animator animator;
-
+	
 	private bool movingRight = false;
 	private bool movingLeft = false;
-	private bool movingUp = false;
-	private bool movingBot = false;
+	private bool movingUp = false;	
 	private bool touchingUp = false;
 	private bool touchingBot = false;
 	private bool touchingLeft = false;
 	private bool touchingRight = false;
 	
 	//	public ChangeSprite changeSprite = new ChangeSprite();
-
+	
 	// Use this for initialization
 	void Start () {
+		//		spriteRenderer = GetComponent<SpriteRenderer> ();
+		//		if (spriteRenderer.sprite == null) {
+		//			spriteRenderer.sprite = gameObject.GetComponent<ChangeSprite>().falling;
+		//		}
 		animator = gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		// TODO: Apply Q-Learning
+		
+		
+		// Apply Reinforced Learning
 		if (moveCount == 0){
-
+			
 			int seed = unchecked(DateTime.Now.Ticks.GetHashCode());
 			System.Random random = new System.Random(seed);
 			float rand = (float) random.NextDouble();
-
+			
+			
+			//Check touchingTop,touchingBot,touchingLeft,touchingRight to set optPossibilities
 			short optPossibilities = checkBorders(transform.position);
-
+			
 			short opt = 0;
 			bool passedTest = false;
-
+			
 			while(!passedTest){
 				opt = (short) Mathf.Ceil((rand * optPossibilities));
 				passedTest = validateOptionAgainstCheckers(opt);
+				//				Debug.Log("Validate:" + passedTest);
+				//				Debug.Log("TouchUp:" + touchingUp + ",TouchRight:" + touchingRight + ",TouchBot:" + touchingBot + ",TouchLeft:" + touchingLeft);
 			}
 			Debug.Log (opt);
-
+			
 			if (opt == 1){
 				//UP
 				movingUp = true;
 				movingRight = false;
 				movingLeft = false;
-				movingBot = false;
+				//				Debug.Log ("UP:" + movingUp + ",RIGHT:" + movingRight + "LEFT:" + movingLeft);
 			}
 			else if (opt == 2){
 				//RIGHT
 				movingUp = false;
 				movingRight = true;
 				movingLeft = false;
-				movingBot = false;
+				//				Debug.Log ("UP:" + movingUp + ",RIGHT:" + movingRight + "LEFT:" + movingLeft);
 			}
 			else if (opt == 3){
 				//BOT
 				movingUp = false;
 				movingRight = false;
 				movingLeft = false;
-				movingBot = true;
+				//				Debug.Log ("UP:" + movingUp + ",RIGHT:" + movingRight + "LEFT:" + movingLeft);
 			}
 			else if (opt == 4){
 				//LEFT
 				movingUp = false;
 				movingRight = false;
 				movingLeft = true;
-				movingBot = false;
+				//				Debug.Log ("UP:" + movingUp + ",RIGHT:" + movingRight + "LEFT:" + movingLeft);
 			}
 		}
-
+		
 		if (moveCount < frameCount){
 			moveCount ++;
 			checkBorders(transform.position);
 			moveCop (movingUp,movingRight,movingLeft);
 		}
-
+		
 		else {
 			moveCount = 0;
+			//Unflag all touchValidators here
 			unflagAllTouchChecks();
 		}
-
-		lookFor(GameObject.Find("player").transform.position);
 	}
 	
 	public void moveCop(bool up, bool right, bool left){
@@ -124,7 +127,7 @@ public class CopMovement : MonoBehaviour {
 					movingLeft = true;
 					right = false;
 					left = true;
-
+					
 				}
 			}
 			else if (left){
@@ -153,7 +156,7 @@ public class CopMovement : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	private void activateAnimation(string animationStr){
 		animator.SetBool("isWalkingUp",false);
 		animator.SetBool("isWalkingRight",false);
@@ -161,7 +164,7 @@ public class CopMovement : MonoBehaviour {
 		animator.SetBool("isWalkingBot",false);
 		animator.SetBool(animationStr,true);
 	}
-
+	
 	private bool validateOptionAgainstCheckers(short opt){
 		if (opt == 1 && touchingUp)
 			return false;
@@ -174,7 +177,7 @@ public class CopMovement : MonoBehaviour {
 		else 
 			return true;
 	}
-
+	
 	// HIC SUNT DRACONES
 	private short checkBorders(Vector3 checkVector)
 	{
@@ -183,110 +186,54 @@ public class CopMovement : MonoBehaviour {
 		Vector3 newPosition = checkVector;
 		Camera mainCamera = Camera.main;
 		Vector3 cameraPosition = mainCamera.transform.position;
-
+		
 		// 2 - X Axis variables
 		float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
 		float xMax = cameraPosition.x + xDist;
 		float xMin = cameraPosition.x - xDist;
-
+		
 		// 3 - Y Axis variables
 		float yDist = mainCamera.orthographicSize;
 		float yMax = mainCamera.transform.position.y + yDist;
 		float yMin = mainCamera.transform.position.y - yDist;
-
+		
+		//		Debug.Log( "xDist:" + xDist + ",xMax:" + xMax + ",xMin:" + xMin + ",yDist:" + yDist + ",yMax:" + yMax + ",yMin:" + yMin 
+		//		          + ",CurrentPosition:" + newPosition.x + "  " + newPosition.y);
+		
 		// 4 - Touching Check
-
+		
 		// Check for X Axis
 		if (newPosition.x <= xMin + 0.3f) {
 			movePossibilities --;
 			touchingLeft = true;
+			//			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			//			moveDirection.x = -moveDirection.x;
 		}
 		else if(newPosition.x >= xMax - 0.3f){
 			movePossibilities --;
 			touchingRight = true;
 		}
-
+		
 		// Check for Y Axis
 		if(newPosition.y <= yMin + 0.3f){
 			movePossibilities --;
 			touchingBot = true;
+			//			newPosition.y = Mathf.Clamp( newPosition.y, -yMax, yMax );
+			//			moveDirection.y = -moveDirection.y;
 		}
 		else if(newPosition.y >= yMax - 0.3f){
 			movePossibilities --;
 			touchingUp = true;
 		}
+		//		transform.position = newPosition;
 		return movePossibilities;
 	}
-
+	
 	private void unflagAllTouchChecks(){
 		touchingUp = false;
 		touchingRight = false;
 		touchingBot = false;
 		touchingLeft = false;
-	}
-
-	private void lookFor(Vector3 objectivePosition){
-
-		float yViewMin;
-		float yViewMax;
-		float xViewMin;
-		float xViewMax;
-		Vector3 currentPosition = transform.position;
-
-		if(movingRight){
-			yViewMin = currentPosition.y - fieldOfViewSides;
-			yViewMax = currentPosition.y + fieldOfViewSides;
-			xViewMin = currentPosition.x;
-			xViewMax = currentPosition.x + fieldOfViewFront;
-			
-			if (objectivePosition.y <= yViewMax && objectivePosition.y >= yViewMin && objectivePosition.x >= xViewMin && objectivePosition.x <= xViewMax){
-				foundSomething();
-			}
-
-		}
-
-		else if(movingLeft){
-			yViewMin = currentPosition.y - fieldOfViewSides;
-			yViewMax = currentPosition.y + fieldOfViewSides;
-			xViewMin = currentPosition.x;
-			xViewMax = currentPosition.x - fieldOfViewFront;
-			
-			if (objectivePosition.y <= yViewMax && objectivePosition.y >= yViewMin && objectivePosition.x <= xViewMin && objectivePosition.x >= xViewMax){
-				foundSomething();
-			}
-		}
-
-		else if(movingUp){
-			yViewMin = currentPosition.y;
-			yViewMax = currentPosition.y + fieldOfViewFront;
-			xViewMin = currentPosition.x  - fieldOfViewSides;
-			xViewMax = currentPosition.x + fieldOfViewSides;
-			
-			if (objectivePosition.y <= yViewMax && objectivePosition.y >= yViewMin && objectivePosition.x >= xViewMin && objectivePosition.x <= xViewMax){
-				foundSomething();
-			}
-		}
-
-		else if(movingBot){
-			yViewMin = currentPosition.y;
-			yViewMax = currentPosition.y - fieldOfViewFront;
-			xViewMin = currentPosition.x - fieldOfViewSides;
-			xViewMax = currentPosition.x + fieldOfViewSides;
-			
-			if (objectivePosition.y >= yViewMax && objectivePosition.y <= yViewMin && objectivePosition.x >= xViewMin && objectivePosition.x <= xViewMax){
-				foundSomething();
-			}
-		}
-	}
-
-	private void foundSomething(){
-		Vector3 instantiateAt = transform.position + new Vector3(0.2f,0.6f,0);
-		GameObject controller = GameObject.Find("controller");
-		if (!controller.GetComponent<PrefabFactory>().instanceExists){
-			controller.GetComponent<PrefabFactory>()
-				.instantiatePrefabBornToDie(controller.GetComponent<PrefabFactory>().prefabArray[0],
-			    	                       instantiateAt,3f);
-		}
 	}
 
 }
