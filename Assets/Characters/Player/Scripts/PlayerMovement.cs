be65a289f8700f0221c4891926b770145aef78f8
keryar <jcamachado@id.uff.br>
2015-11-21 23:24:56 -0800

    Interactions and animation correction.
    Added some script code to PlayerMovement about interactions and fixed
    the animation.

--- Assets/Characters/Player/Scripts/PlayerMovement.cs
+++ Assets/Characters/Player/Scripts/PlayerMovement.cs
@@ -2,8 +2,17 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-	
-	public SpriteRenderer spriteRenderer;
+    //face states
+    private bool standingUp = false;
+    private bool standingDown = false;
+    private bool standingLeft = false;
+    private bool standingRight = false;
+
+    //interactions
+    private bool touchingWall = false;
+
+
+    public SpriteRenderer spriteRenderer;
 	public float moveSpeed = 0.5f;
 	public float jumpHeight = 5.0f;
 
@@ -17,6 +26,7 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (Input.GetKey (KeyCode.D)) {
 			if (!isWalking){
 				activateAnimation("isWalkingRight");
@@ -50,11 +60,55 @@ public class PlayerMovement : MonoBehaviour {
 			
 		}
 
-		if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W)){
-			activateAnimation("isStanding");
-			isWalking = false;
-		}
-	}
+        /*	if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.W)){
+                activateAnimation("isStanding");
+                isWalking = false;
+            }
+            */
+
+        //possible use of switch case keycode.variable case variable A,S...
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            activateAnimation("isStanding");
+            setDirection("left");
+            setWalk(false);
+            //isWalking = false;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            activateAnimation("isStanding");
+            setDirection("down");
+            setWalk(false);
+            //isWalking = false;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            activateAnimation("isStanding");
+            setDirection("right");
+            setWalk(false);
+            //isWalking = false;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            activateAnimation("isStanding");
+            setDirection("up");
+            setWalk(false);
+            //isWalking = false;
+
+
+        }
+
+
+        if ((Input.GetKey(KeyCode.Space)) /*&& touch*/)
+        {
+            interact();
+            
+        }
+        getFace();
+    }
+
 	
 	private void flip(){
 		gameObject.transform.localScale = new Vector3((gameObject.transform.localScale.x * (-1)), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
@@ -68,5 +122,117 @@ public class PlayerMovement : MonoBehaviour {
 		animator.SetBool("isStanding",false);
 		animator.SetBool(animationStr,true);
 	}
+
+
+    //testing the idea in the same script
+    //aim to referr to another object
+
+    private void interact()
+    {
+        Debug.Log("Touched");
+    }
+
+    public void setDirection(string dir)
+    {
+        if (dir == "up")
+        {
+            standingUp = true;
+            standingRight = false;
+            standingLeft = false;
+            standingDown = false;
+        }
+        else if (dir == "left")
+        {
+
+            standingUp = false;
+            standingRight = false;
+            standingLeft = true;
+            standingDown = false;
+        }
+        else if (dir == "down")
+        {
+
+            standingUp = false;
+            standingRight = false;
+            standingLeft = false;
+            standingDown = true;
+        }
+        else if (dir == "right")
+        {
+
+            standingUp = false;
+            standingRight = true;
+            standingLeft = false;
+            standingDown = false;
+        }
+        else
+        {
+            Debug.Log("Wrong string");
+        }
+
+
+    }
+    public void setWalk(bool b)
+    {
+        isWalking = b;
+    }
+
+    public int getFace()
+    {
+        if (standingDown)
+        {
+            Debug.Log("down");
+            return 1;
+        }
+        else if (standingLeft)
+        {
+            Debug.Log("left");
+            return 2;
+        }
+        else if (standingRight)
+        {
+            Debug.Log("right");
+            return 3;
+        }
+        else if (standingUp)
+        {
+            Debug.Log("up");
+            return 4;
+        }
+        else
+        {
+            Debug.Log("no direction");
+            return 0;
+        }
+
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+      if (collision.name == "wall")
+            
+        {
+        touchingWall = true;
+            Debug.Log(touchingWall + "TW");
+        //collision.gameObject.SendMessage("in front of the wall")`
+        //paint(gameObject);
+      }
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.name == "wall")
+
+        {
+            touchingWall = false;
+            Debug.Log(touchingWall + "TW false");
+            //collision.gameObject.SendMessage("in front of the wall")`
+            //paint(gameObject);
+        }
+    }
+
 
 }
