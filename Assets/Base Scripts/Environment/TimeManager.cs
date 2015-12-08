032575cf5fe815d98415d96ebd783ea142b84e4b
chups2themax <osuna.9@gmail.com>
2015-12-08 03:48:23 -0800

    Added Time Clock and Pause Menu
    Pause Menu errors, does not stop all character movements.

---
+++ Assets/Base Scripts/Environment/TimeManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour {
+
+    public float startTime;
+    private float counterTime;
+    private Text timeCounter;
+    private PauseScript pauseScene;
+
+
+	// Use this for initialization
+	void Start () {
+        counterTime = startTime;
+        pauseScene = FindObjectOfType<PauseScript>();
+        timeCounter = GetComponent<Text>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(pauseScene.isPaused)
+        {
+            return;
+        }
+
+        counterTime -= Time.deltaTime;
+        if(counterTime <= 0)
+        {
+            Application.LoadLevel("loseScene");
+            //RestTime();
+        }
+        timeCounter.text = "" + Mathf.Round(counterTime);
+	}
+
+    /*
+    public void RestTime()
+    {
+        counterTime = startTime;
+    }
+    */
+}
